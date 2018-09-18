e2e451996493c7eafbbad7c8a47b3608750ea8c5
Michał Sieński <michal.sienski1987@gmail.com>
2018-09-18 20:34:43 +0200

    Dodanie plikow z kursu

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req, res) => {
+  const wes = {
+    name: 'Michał',
+    wiek: 31,
+    cool: true
+  };
+  // res.send('Hey! It works!');
+  // res.json(wes)
+  res.send(req.query)
+});
+
+router.get('/reverse/:name', (req,res)=>{
+  const reverse = [...req.params.name].reverse().join('')
+  res.send(reverse);
+})
+
+module.exports = router;
